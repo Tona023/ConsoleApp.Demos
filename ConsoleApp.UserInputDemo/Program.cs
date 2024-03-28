f4a3c5c55c6ff44a42b6af460eca2dddddbc378e
Tona023 <uniquelylatona23@gmail.com>
2024-03-28 18:54:31 -0500

    Added first and last name prompt.

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,11 +1,18 @@
 ﻿//Declare variables
-string name = string.Empty;
+using System.Xml.Linq;
+
+string FirstName = string.Empty;
+string LastName = string.Empty;
+
 int age = 0;
 int RetirementAge = 65;
 
 //Prompt the user for input
-Console.WriteLine("Please enter your name.");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your first name.");
+FirstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name.");
+LastName = Console.ReadLine();
 
 Console.WriteLine("Please enter your age.");
 //string initiaAgeValue = Console.ReadLine();
@@ -15,7 +22,7 @@ age = Convert.ToInt32(Console.ReadLine());
 int workingYearsRemaining = RetirementAge - age;
 
 // Output the results to the user
-Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Full name: {FirstName} {LastName}");
 Console.WriteLine($"Age: {age}");
 Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
 
